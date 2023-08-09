64366f4059bbe2d4f3613809b9e5bdc29ce83814
Benjamin Falkner <bfalkner9@gmail.com>
2023-08-09 12:43:17 -0400

    image carousel in home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,25 @@ export default function Home() {
 
   return (
     <main>
-     {/*THIS IS THE LANDING PAGE*/}
-    <p>Hello world</p>
+      <div className="flex overflow-x-auto">
+        <div className="flex-none">
+          <img className="h-48 w-96 object-cover" src="/car-temp1.png" />
+        </div>
+        <div className="flex-none">
+          <img className="h-48 w-96 object-cover" src="/car-temp2.png" />
+        </div>
+        <div className="flex-none">
+          <img className="h-48 w-96 object-cover" src="/car-temp3.jpeg" />
+        </div>
+        <div className="flex-none">
+          <img className="h-48 w-96 object-cover" src="/car-temp1.png" />
+        </div>
+        <div className="flex-none">
+          <img className="h-48 w-96 object-cover" src="/car-temp2.png" />
+        </div>
+      </div>
+
+      
     </main>
   )
 }
