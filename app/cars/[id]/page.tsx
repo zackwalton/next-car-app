3fc03c28b5749aec672ba6446da2fb1e221ad754
chrisegener22 <78327583+chrisegener22@users.noreply.github.com>
2023-08-14 22:55:21 -0400

    added details to car page
    No fully complete, this goofy ass text makes it look like ass

--- app/cars/[id]/page.tsx
+++ app/cars/[id]/page.tsx
@@ -25,7 +25,21 @@ export default function CarPage({ params }: Props) {
   return (
     <main>
       <div className={"flex bg-[url('/bg.jpg')] overflow-hidden justify-center items-center"}  >
-      < Image className={"border border-gray-300 rounded-lg"} src={myCar.image} alt={"None"} height={500} width={500}/> 
+        <div className='flex flex-col space-y-2'>
+          < Image className={"border border-gray-300 rounded-lg"} src={myCar.image} alt={"None"} height={500} width={500}/> 
+          <p className='text-white text-4xl'>{myCar.name}</p>
+          <p className='text-white text-4xl'>${myCar.price}</p>
+          <p className='text-white text-4xl'>{myCar.Kilometers} km</p>
+          <p className='text-white text-4xl'>Inventory  {myCar.inventory}</p>
+          <p className='text-white text-xl'>{myCar.specs}</p>
+          <p className='text-white text-xl'>{myCar.Accidents}</p>
+          <p className='text-white text-xl'>{myCar.Maintenance}</p>
+          <p className='text-white text-xl'>{myCar.Mods}</p>
+          <p className='text-white text-xl'>{myCar.Condition}</p>
+          <p className='text-white text-xl'>{myCar.SaleType}</p>
+          <p className='text-white text-xl'>{myCar.DeliveryType}</p>
+
+        </div>
     </div>
     </main>
   )
