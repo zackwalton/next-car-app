c7771c69bb7feca0de96d07b0a050e6e58943945
Benjamin Falkner <bfalkner9@gmail.com>
2023-08-19 20:42:21 -0400

    made navbar transparent

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
 
   return (
-    <nav className="w-full h-24 shadow-xl bg-zinc-900">
+    <nav className="w-full h-24 shadow-xl bg-[url('/bg.jpg')]">
         <div className="flex justify-between items-center h-full w-full px-4 2xl:px-16">
             <Link href='/'>
                 <Image
@@ -36,7 +36,7 @@ const Navbar = () => {
                     <Link href="/">
                         <li className='ml-10 uppercase hover:border-b text-xl text-white'>Home</li>
                     </Link>
-                    <Link href="/marketplace">
+                    <Link href="marketplace">
                         <li className='ml-10 uppercase hover:border-b text-xl text-white'>Market</li>
                     </Link>
                     <Link href="contact-us">
@@ -47,11 +47,11 @@ const Navbar = () => {
                     </Link>
                 </ul>
             </div>
-            <div onClick={handleNav} className="md:hidden cursor-pointer pl-24 bg-zinc-900 text-white hover:border-w">
+            <div onClick={handleNav} className="md:hidden cursor-pointer pl-24 bg-black text-white hover:border-w">
                 <AiOutlineMenu size={30} />
             </div>
         </div>
-        <div className={menuOpen ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-black p-10 ease-in duration-500" : "fixed left-[-100%] top-0 p-10 ease-in duration-500"}>
+        <div className={menuOpen ? "fixed left-0 top-0 w-[65%] sm:hidden h-screen bg-black p-10 ease-in duration-500" : "fixed left-[-100%] top-0 p-10 ease-in duration-500 "}>
             <ul>
                 <Link href="/" onClick={handleNav}>
                     <li className=' uppercase hover:border-b text-xl text-white'>Home</li>
